483fa1cf051fbc8b907eeadbdd0fddf945a95c7b
domie163 <domie1948@163.com>
2025-06-10 16:21:55 +0800

    Initial commit

---
+++ youxizhanghao/springbooty2rp6/src/main/resources/front/front/src/config/config.js
@@ -0,0 +1,21 @@
+export default {
+    baseUrl: 'http://localhost:8080/springbooty2rp6/',
+    indexNav: [
+        {
+            name: '首页',
+            url: '/index/home'
+        },
+        {
+            name: '账号信息',
+            url: '/index/shangpinxinxi'
+        },
+        {
+            name: '秒杀账号',
+            url: '/index/miaoshashangpin'
+        },
+        {
+            name: '平台资讯',
+            url: '/index/news'
+        },
+    ]
+}
